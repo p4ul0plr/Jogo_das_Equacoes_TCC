ef3735c5a7d61adce010699e43d2198a28291cd7
Paulo Nobrega <paulo.nobrega@otimizeit.com.br>
2021-05-18 17:26:14 -0300

    Ajustes de textos na página de ajuda

--- lib/screens/help_page.dart
+++ lib/screens/help_page.dart
@@ -34,74 +34,6 @@ class HelpPage extends StatelessWidget {
     );
   }
 
-  InfoCard _podiumCard() {
-    return InfoCard(
-      color: ThemeColors().lightPink,
-      text: [
-        'A pontuação adquirida durante o jogo será usada para a construção do pódio onde você poderá comparar a sua pontuação com os colegas e amigos',
-      ],
-      imagePath: 'assets/images/screens/podium.jpg',
-      infoCardPosition: InfoCardPosition.textRightImageLeft,
-    );
-  }
-
-  InfoCard _loseCard() {
-    return InfoCard(
-      color: ThemeColors().lightPink,
-      text: [
-        'Mas se você não conseguir passar na missão não se preocupe, você pode reiniciar a missão e tentar quantas vezes quiser'
-      ],
-      imagePath: 'assets/images/screens/failure.jpg',
-      infoCardPosition: InfoCardPosition.textLeftImageRight,
-    );
-  }
-
-  InfoCard _winCard() {
-    return InfoCard(
-      color: ThemeColors().lightPink,
-      text: [
-        'A cada missão concluída, você receberá uma pontuação condizente com o seu desempenho',
-        'Essa pontuação será incrementada e formará a sua pontuação total',
-      ],
-      imagePath: 'assets/images/screens/success.jpg',
-      infoCardPosition: InfoCardPosition.textRightImageLeft,
-    );
-  }
-
-  InfoCard _gameMatchCard() {
-    return InfoCard(
-      color: ThemeColors().lightPink,
-      text: [
-        'A cada missão você receberá uma equação diferente para resolver',
-        'Você terá 1 minuto para escolher a resposta correta',
-        'Você terá 3 chances para acertar a resposta dentre as alternativas',
-      ],
-      imagePath: 'assets/images/screens/quests/quest-38.jpg',
-      infoCardPosition: InfoCardPosition.textLeftImageRight,
-    );
-  }
-
-  InfoCard _questsCard() {
-    return InfoCard(
-      color: ThemeColors().lightPink,
-      text: [
-        'Cada fase contém 10 missões diferentes',
-        'A cada missão concluida, a seguinte será desbloqueada',
-      ],
-      imagePath: 'assets/images/screens/quests.jpg',
-      infoCardPosition: InfoCardPosition.textRightImageLeft,
-    );
-  }
-
-  InfoCard _stagesCard() {
-    return InfoCard(
-      color: ThemeColors().lightPink,
-      text: ['O jogo contém 4 fases com níveis de dificuldade diferente'],
-      imagePath: 'assets/images/screens/stages.jpg',
-      infoCardPosition: InfoCardPosition.textLeftImageRight,
-    );
-  }
-
   CustomCard _titleCard() {
     return CustomCard(
       color: ThemeColors().lightPink,
@@ -117,14 +49,71 @@ class HelpPage extends StatelessWidget {
     );
   }
 
-  InfoCard _accountCard() {
+  InfoCard _stagesCard() {
+    return InfoCard(
+      color: ThemeColors().lightPink,
+      text: ['O jogo contém 4 fases com níveis de dificuldade diferente.'],
+      imagePath: 'assets/images/screens/stages.jpg',
+      infoCardPosition: InfoCardPosition.textLeftImageRight,
+    );
+  }
+
+  InfoCard _questsCard() {
     return InfoCard(
       color: ThemeColors().lightPink,
       text: [
-        'Para participar do pódio é necessário acesso a internet e a criação da conta com as informações do usuário',
+        'Cada fase contém 10 missões diferentes.',
+        'A cada missão concluida, a seguinte será desbloqueada.',
       ],
-      imagePath: 'assets/images/screens/account.jpg',
+      imagePath: 'assets/images/screens/quests.jpg',
+      infoCardPosition: InfoCardPosition.textRightImageLeft,
+    );
+  }
+
+  InfoCard _gameMatchCard() {
+    return InfoCard(
+      color: ThemeColors().lightPink,
+      text: [
+        'A cada missão você receberá uma equação diferente para resolver.',
+        'Você terá 60 segundos para escolher a resposta correta.',
+        'Você terá 3 chances para acertar a resposta dentre as alternativas.',
+      ],
+      imagePath: 'assets/images/screens/quests/quest-38.jpg',
       infoCardPosition: InfoCardPosition.textLeftImageRight,
+    );
+  }
+
+  InfoCard _winCard() {
+    return InfoCard(
+      color: ThemeColors().lightPink,
+      text: [
+        'A cada missão concluída, você receberá uma pontuação condizente com o seu desempenho.',
+        'Essa pontuação será incrementada e formará a sua pontuação total.',
+      ],
+      imagePath: 'assets/images/screens/success.jpg',
+      infoCardPosition: InfoCardPosition.textRightImageLeft,
+    );
+  }
+
+  InfoCard _loseCard() {
+    return InfoCard(
+      color: ThemeColors().lightPink,
+      text: [
+        'Mas se você não conseguir passar na missão não se preocupe, você pode reiniciar a missão e tentar quantas vezes quiser.'
+      ],
+      imagePath: 'assets/images/screens/failure.jpg',
+      infoCardPosition: InfoCardPosition.textLeftImageRight,
+    );
+  }
+
+  InfoCard _podiumCard() {
+    return InfoCard(
+      color: ThemeColors().lightPink,
+      text: [
+        'A pontuação adquirida durante o jogo será usada para a construção do pódio onde você poderá comparar a sua pontuação com os colegas e amigos.',
+      ],
+      imagePath: 'assets/images/screens/podium.jpg',
+      infoCardPosition: InfoCardPosition.textRightImageLeft,
     );
   }
 
@@ -137,6 +126,17 @@ class HelpPage extends StatelessWidget {
       ],
       imagePath: 'assets/images/screens/login.jpg',
       infoCardPosition: InfoCardPosition.textRightImageLeft,
+    );
+  }
+
+  InfoCard _accountCard() {
+    return InfoCard(
+      color: ThemeColors().lightPink,
+      text: [
+        'Para participar do pódio é necessário acesso a internet e a criação da conta com as informações do usuário.',
+      ],
+      imagePath: 'assets/images/screens/account.jpg',
+      infoCardPosition: InfoCardPosition.textLeftImageRight,
     );
   }
 }
